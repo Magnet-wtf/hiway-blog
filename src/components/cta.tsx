2715478baa323e3d34876738a777f20803d061cf
Jeremy Scatigna <scatignaj@me.com>
2024-02-19 14:35:16 +0100

    feat(single-post): fix titles and content

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -7,8 +7,8 @@ export default function CTA() {
         <div className='w-full bg-[#3C68FF] rounded-xl min-h-[150px] px-12 flex items-center justify-center mt-12'>
             <div className='w-full flex items-center justify-between h-full'>
                 <div className='flex flex-col items-start justify-center text-start h-full pt-8'>
-                    <h1 className='text-white text-2xl font-bold'>Simule ton revenu de freelance</h1>
-                    <p className='text-white font-thin'>
+                    <div className='text-white text-2xl font-bold'>Simule ton revenu de freelance</div>
+                    <p className='text-white text-sm font-thin'>
                         En moyenne, les freelances Hiway gagnent 7100 € <br /> nets / mois avant IR
                     </p>
                 </div>
