cfa24b9268464d6d2123b8e4f95ee7bdff437859
Jeremy Scatigna <scatignaj@me.com>
2024-02-20 15:57:22 +0100

    feat(responsive): header cleanup and list padding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,6 +14,7 @@ import {
     NavigationMenuList,
     NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu';
+import { isMobile } from 'react-device-detect';
 
 export default function Header() {
     return (
@@ -22,54 +23,55 @@ export default function Header() {
                 <Link href='/'>
                     <Image src='/logo.png' alt='logo' width={45} height={45} />
                 </Link>
+                {!isMobile && (
+                    <div className='flex space-x-4 items-center'>
+                        <NavigationMenu className='pt-8'>
+                            <NavigationMenuList>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger>Accompagnement</NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        <div className='md:w-[300px] lg:w-[400px]'>
+                                            <ListItem href='https://hiway.fr/devenir-freelance' title="On t'aide à lancer ton activité" />
+                                            <ListItem
+                                                href='https://hiway.fr/plateforme-accompagnement-freelance'
+                                                title='On gère ta société au quotidien'
+                                            />
+                                            <ListItem
+                                                href='https://hiway.fr/investir-patrimoine-freelance'
+                                                title="On t'aide à bien préparer ton avenir"
+                                            />
+                                        </div>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <Link href='/temoignage' className='font-light text-sm'>
+                                        Témoignages
+                                    </Link>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger>Ressources</NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        <div className='md:w-[300px] lg:w-[400px]'>
+                                            <ListItem
+                                                href='https://hiway.fr/simulateur?s=salarieClientFinal'
+                                                title='Simulation revenu de freelance'
+                                            />
+                                            <ListItem href='/' title='Blog' />
+                                        </div>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                            </NavigationMenuList>
+                        </NavigationMenu>
 
-                <div className='flex space-x-4 items-center'>
-                    <NavigationMenu className='pt-8'>
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <NavigationMenuTrigger>Accompagnement</NavigationMenuTrigger>
-                                <NavigationMenuContent>
-                                    <div className='md:w-[300px] lg:w-[400px]'>
-                                        <ListItem href='https://hiway.fr/devenir-freelance' title="On t'aide à lancer ton activité" />
-                                        <ListItem
-                                            href='https://hiway.fr/plateforme-accompagnement-freelance'
-                                            title='On gère ta société au quotidien'
-                                        />
-                                        <ListItem
-                                            href='https://hiway.fr/investir-patrimoine-freelance'
-                                            title="On t'aide à bien préparer ton avenir"
-                                        />
-                                    </div>
-                                </NavigationMenuContent>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <Link href='/temoignage' className='font-light text-sm'>
-                                    Témoignages
-                                </Link>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <NavigationMenuTrigger>Ressources</NavigationMenuTrigger>
-                                <NavigationMenuContent>
-                                    <div className='md:w-[300px] lg:w-[400px]'>
-                                        <ListItem
-                                            href='https://hiway.fr/simulateur?s=salarieClientFinal'
-                                            title='Simulation revenu de freelance'
-                                        />
-                                        <ListItem href='/' title='Blog' />
-                                    </div>
-                                </NavigationMenuContent>
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenu>
-
-                    <Button className='rounded-full bg-[#F96547]'>Nous contacter</Button>
-                    <Button variant='outline' className='rounded-full border-[#f96547] border-2'>
-                        Simuler mon revenue
-                    </Button>
-                    <Button className='rounded-full px-2 bg-[#F96547]'>
-                        <User className='h-6 w-6' />
-                    </Button>
-                </div>
+                        <Button className='rounded-full bg-[#F96547]'>Nous contacter</Button>
+                        <Button variant='outline' className='rounded-full border-[#f96547] border-2'>
+                            Simuler mon revenue
+                        </Button>
+                        <Button className='rounded-full px-2 bg-[#F96547]'>
+                            <User className='h-6 w-6' />
+                        </Button>
+                    </div>
+                )}
             </div>
         </div>
     );
