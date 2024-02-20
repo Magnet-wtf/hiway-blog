371f48d4e29c3a9436d50d482db2e3e9d8166bee
Jeremy Scatigna <scatignaj@me.com>
2024-02-20 16:04:14 +0100

    feat(responsive): fix mobile header title

--- src/components/mobile-header.tsx
+++ src/components/mobile-header.tsx
@@ -9,8 +9,8 @@ export default function MobileHeader() {
 
     return (
         <div className="flex flex-col mb-8">
-            <h1 className={'text-2xl xl:text-4xl font-bold font-jekoblack'}>
-                <span className='text-[#FF4140] font-jekoblack'>Bienvenue</span> <br /> sur le blog <br /> Hiway
+            <h1 className={'text-4xl font-bold font-jekoblack'}>
+                <span className='text-[#FF4140] font-jekoblack'>Bienvenue</span> sur le blog Hiway
             </h1>
             <p className={'text-sm mt-4 text-[#979797]'}>Tout ce qu’il faut savoir pour vivre sa meilleure vie de freelance</p>
         </div>
