bc7c49e782a5459b3336a48e87a92a368428c90a
Jeremy Scatigna <scatignaj@me.com>
2024-02-20 16:15:49 +0100

    feat(responsive): mobile menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from './ui/button';
-import { Menu, User } from 'lucide-react';
+import { Cross, Menu, User } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import React from 'react';
 import {
@@ -15,8 +15,10 @@ import {
     NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu';
 import { isMobile } from 'react-device-detect';
+import { Dialog, DialogContent } from './ui/dialog';
 
 export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
     return (
         <div className={'w-full'}>
             <div className='w-full h-[56px] p-4 flex items-center justify-between shadow-xl'>
@@ -71,13 +73,56 @@ export default function Header() {
                             <User className='h-6 w-6' />
                         </Button>
                     </div>
-                ): (
+                ) : (
                     <div className='flex space-x-4 items-center'>
-                        <Menu />
+                        <Menu onClick={() => setMobileMenuOpen(true)} />
                     </div>
-                
                 )}
             </div>
+            <Dialog open={mobileMenuOpen} onOpenChange={() => setMobileMenuOpen(!mobileMenuOpen)}>
+                <div className='fixed inset-0 z-50' />
+                <DialogContent className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm'>
+                    <div className='flex items-center justify-between'>
+                        <Link href='/' className='-m-1.5 p-1.5'>
+                            <Image src='/logo.png' alt='logo' width={45} height={45} />
+                        </Link>
+                        <button type='button' className='-m-2.5 rounded-md p-2.5 text-white' onClick={() => setMobileMenuOpen(false)}>
+                            <span className='sr-only'>Fermer</span>
+                            <Cross className='h-6 w-6' aria-hidden='true' />
+                        </button>
+                    </div>
+                    <div className='mt-6 flow-root'>
+                        <div className='-my-6'>
+                            <div className='space-y-2 py-6'>
+                                <ListItem href='https://hiway.fr/devenir-freelance' title="On t'aide à lancer ton activité" />
+                                <ListItem
+                                    href='https://hiway.fr/plateforme-accompagnement-freelance'
+                                    title='On gère ta société au quotidien'
+                                />
+                                <ListItem
+                                    href='https://hiway.fr/investir-patrimoine-freelance'
+                                    title="On t'aide à bien préparer ton avenir"
+                                />
+                                <Link href='/temoignage' className='font-light text-sm'>
+                                    Témoignages
+                                </Link>
+                                <ListItem href='https://hiway.fr/simulateur?s=salarieClientFinal' title='Simulation revenu de freelance' />
+                                <ListItem href='/' title='Blog' />
+                            </div>
+
+                            <div className='py-6'>
+                                <Button className='rounded-full bg-[#F96547]'>Nous contacter</Button>
+                                <Button variant='outline' className='rounded-full border-[#f96547] border-2'>
+                                    Simuler mon revenue
+                                </Button>
+                                <Button className='rounded-full px-2 bg-[#F96547]'>
+                                    <User className='h-6 w-6' />
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
