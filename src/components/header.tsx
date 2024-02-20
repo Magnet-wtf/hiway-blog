a5bc0fc9aaf45c8a072d439149fb67bf65a3534e
Jeremy Scatigna <scatignaj@me.com>
2024-02-20 16:55:00 +0100

    feat(responsive): icon color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
             <div className='w-full h-[56px] p-4 flex items-center justify-between shadow-xl'>
                 {isMobile && (
                     <Button className='rounded-full px-2 bg-transparent'>
-                        <User className='h-6 w-6' />
+                        <User className='h-6 w-6 text-black' />
                     </Button>
                 )}
                 <Link href='/'>
