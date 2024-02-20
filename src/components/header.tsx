9360fff5b69554265880b91ebac7da3a8b4185e8
Jeremy Scatigna <scatignaj@me.com>
2024-02-20 16:45:57 +0100

    feat(responsive): try removing the icon on mobile menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -92,15 +92,12 @@ export default function Header() {
             {mobileMenuOpen && (
                 <div className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm'>
                     <div className='flex items-center justify-between'>
-                        <Link href='/' className='-m-3'>
-                            <Image src='/logo.png' alt='logo' width={45} height={45} />
-                        </Link>
                         <button type='button' className='-m-2.5 rounded-md p-2.5 text-black' onClick={() => setMobileMenuOpen(false)}>
                             <span className='sr-only'>Fermer</span>
                             <X className='h-6 w-6' aria-hidden='true' />
                         </button>
                     </div>
-                    <div className='mt-6 flow-root'>
+                    <div className='-m-2.5 flow-root'>
                         <div className='-my-6'>
                             <div className='space-y-2 py-6 flex flex-col'>
                                 <Link href='https://hiway.fr/devenir-freelance' className='text-lg font-medium'>
