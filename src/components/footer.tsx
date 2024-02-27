6715b9e5c6c044c545a70f0dfe84efecb8aba36b
Jeremy Scatigna <scatignaj@me.com>
2024-02-27 14:47:10 +0100

    feat(updates): footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -20,9 +20,9 @@ export default function Footer() {
                             <div className='text-4xl font-jekobold leading-24'>
                                 Plus de 700 freelances <br /> ont changé de vie <br /> avec Hiway
                             </div>
-                            <h1 className='text-lg font-jekobold text-left mt-8'>
+                            <div className='text-lg font-jekobold text-left mt-8'>
                                 Devenir freelance n&apos;a jamais été aussi simple qu&apos;avec Hiway.
-                            </h1>
+                            </div>
                             <p className='text-sm font-medium text-left mt-8'>
                                 On t&apos;accompagne à chaque étape, on gère tout pour que tu profites au maximum de ton statut et que tu
                                 t&apos;ouvres encore plus de perspectives.
@@ -47,12 +47,12 @@ export default function Footer() {
                 ) : (
                     <div className='w-full rounded-xl bg-[#F6F6F6] grid grid-cols-2 min-h-[628px]'>
                         <div className='flex flex-col items-start justify-start text-left pt-24 pl-24 h-full w-full'>
-                            <h1 className='text-6xl font-jekobold leading-24'>
+                            <div className='text-6xl font-jekobold leading-24'>
                                 Plus de 700 freelances <br /> ont changé de vie <br /> avec Hiway
-                            </h1>
-                            <h1 className='text-lg font-jekobold text-left mt-8'>
+                            </div>
+                            <div className='text-lg font-jekobold text-left mt-8'>
                                 Devenir freelance n&apos;a jamais été aussi simple qu&apos;avec Hiway.
-                            </h1>
+                            </div>
                             <p className='text-sm font-medium text-left mt-8'>
                                 On t&apos;accompagne à chaque étape, on gère tout pour que tu profites au maximum de ton statut et que tu
                                 t&apos;ouvres encore plus de perspectives.
@@ -86,10 +86,10 @@ export default function Footer() {
                         <div className={`flex flex-col justify-center items-center ${isMobile ? "p-4" : "p-12"} w-full`}>
                             <div className='flex flex-col items-start justify-start text-left h-full w-full'>
                                 <Image src={'/logo-big.png'} alt='icon' width={100} height={100} />
-                                <h1 className='text-2xl font-jekoblack leading-24 mt-8'>
+                                <div className='text-2xl font-jekoblack leading-24 mt-8'>
                                     La meilleure expérience
                                     pour devenir freelance
-                                </h1>
+                                </div>
                                 <Link
                                     href={'https://hiway.fr/simulateur?s=salarieClientFinal'}
                                     className='rounded-full border-[#FF4140] border-2 px-4 py-4 flex justify-center items-center mt-8 bg-white font-jekobold text-sm'
@@ -98,14 +98,14 @@ export default function Footer() {
                                 </Link>
                             </div>
                             <div className='flex flex-col items-start justify-start h-full w-full space-y-4 mt-8'>
-                                <h1 className='font-jekoblack'>Accompagnement</h1>
+                                <div className='font-jekoblack'>Accompagnement</div>
                                 <p>On t&apos;aide à lancer ton activité</p>
                                 <p>On gère ta société au quotidien</p>
                                 <p>On t&apos;aide à bien préparer ton avenir</p>
                             </div>
                             <div className='flex flex-col items-start justify-start h-full w-full space-y-4 mt-8'>
                                 <div className='flex flex-col w-full items-start justify-start space-y-4 pb-12'>
-                                    <h1 className='font-jekoblack'>Ressources</h1>
+                                    <div className='font-jekoblack'>Ressources</div>
                                     <p className=''>Témoignages</p>
                                     <p>Simuler mon revenu</p>
                                     <p>Blog</p>
@@ -122,10 +122,10 @@ export default function Footer() {
                         <div className='grid grid-cols-3 h-full p-24 min-h-[507px] w-full justify-items-center'>
                             <div className='flex flex-col items-start justify-start text-left h-full w-full'>
                                 <Image src={'/logo-big.png'} alt='icon' width={100} height={100} />
-                                <h1 className='text-2xl font-jekoblack leading-24 mt-8'>
+                                <div className='text-2xl font-jekoblack leading-24 mt-8'>
                                     La meilleure expérience <br />
                                     pour devenir freelance
-                                </h1>
+                                </div>
                                 <Link
                                     href={'https://hiway.fr/simulateur?s=salarieClientFinal'}
                                     className='rounded-full border-[#FF4140] border-2 px-4 py-4 flex justify-center items-center mt-8 bg-white font-jekobold'
@@ -134,14 +134,14 @@ export default function Footer() {
                                 </Link>
                             </div>
                             <div className='flex flex-col items-start px-12 justify-start h-full w-full space-y-4'>
-                                <h1 className='font-jekoblack'>Accompagnement</h1>
+                                <div className='font-jekoblack'>Accompagnement</div>
                                 <p>On t&apos;aide à lancer ton activité</p>
                                 <p>On gère ta société au quotidien</p>
                                 <p>On t&apos;aide à bien préparer ton avenir</p>
                             </div>
                             <div className='flex flex-col items-start justify-start space-y-20 px-12'>
                                 <div className='flex flex-col w-full items-start justify-start space-y-4'>
-                                    <h1 className='font-jekoblack'>Ressources</h1>
+                                    <div className='font-jekoblack'>Ressources</div>
                                     <p className=''>Témoignages</p>
                                     <p>Simuler mon revenu</p>
                                     <p>Blog</p>
