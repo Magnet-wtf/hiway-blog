e17eba42a39367d4cb510f73b153a806a76e3f9b
Jeremy Scatigna <scatignaj@me.com>
2024-03-04 09:42:00 +0100

    feat(updates): updates

--- src/components/post-content.tsx
+++ src/components/post-content.tsx
@@ -12,8 +12,10 @@ import parse, { domToReact } from 'html-react-parser';
 import he from 'he';
 import { WP_REST_API_Term } from 'wp-types';
 import { isMobile } from 'react-device-detect';
-import { createRef, useRef } from 'react';
+import { createRef, useCallback, useRef, useState } from 'react';
 import DialogForm from './form-dialog';
+import Axios from 'axios';
+import wpService from '@/lib/wordpress/wp-service';
 
 export default function PostContent({
     post,
@@ -26,6 +28,7 @@ export default function PostContent({
     tag: WP_REST_API_Term;
     filteredPosts: PostWithMedia[];
 }) {
+    const [isCommentSent, setIsCommentSent] = useState(false);
     const options = {
         replace({ attribs, children }: any) {
             if (!attribs) {
@@ -68,6 +71,35 @@ export default function PostContent({
             return posts;
         }
     };
+
+    let fields = {
+        author_name: '',
+        author_email: '',
+        content: '',
+        post: post.id, //getting this from the main component
+    };
+
+    const fieldChangeHandler = (e: any) => {
+        switch (e.target.name) {
+            case 'commenter-name':
+                fields.author_name = e.target.value;
+                break;
+            case 'commenter-email':
+                fields.author_email = e.target.value;
+                break;
+            case 'commenter-message':
+                fields.content = e.target.value;
+                break;
+        }
+    };
+
+    const handleSubmit = useCallback(async (e: any) => {
+        e.preventDefault();
+        setIsCommentSent(true);
+        let response = await wpService.postComment(post.id, fields.author_name, fields.author_email, fields.content);
+        console.log(response);
+    }, []);
+
     return (
         <div className={`flex flex-col w-full justify-center space-y-4 ${!isMobile ? 'px-24' : 'px-8'} py-12`} id='toPDF'>
             <DialogForm />
@@ -120,31 +152,50 @@ export default function PostContent({
                 <CTA />
 
                 <div className='w-full flex flex-col items-center justify-center mt-12'>
-                    <div
-                        className={`w-full rounded-xl bg-[#F6F6F6] ${
-                            isMobile ? 'p-4' : 'p-12'
-                        } flex flex-col justify-center items-center text-center`}
-                    >
-                        <div className='font-jekobold text-black text-2xl'>
-                            Une question ? Une remarques ? <br />{' '}
-                            <span className='text-[#ff4140] font-jekobold'>Laisse un commentaire</span>
-                        </div>
-                        <div className='flex flex-col items-center justify-center mt-8 w-full'>
-                            <div className='flex items-center space-x-8 w-full'>
-                                <Input placeholder='Ton nom' className='rounded-full bg-[#F6F6F6] border-[#979797] border p-8' />
-                                <Input placeholder='Ton email' className='rounded-full bg-[#F6F6F6] border-[#979797] border p-8' />
+                    {isCommentSent ? (
+                        <div>Merci pour ton commentaire, il est en attente de modération</div>
+                    ) : (
+                        <form
+                            onSubmit={handleSubmit}
+                            className={`w-full rounded-xl bg-[#F6F6F6] ${
+                                isMobile ? 'p-4' : 'p-12'
+                            } flex flex-col justify-center items-center text-center`}
+                        >
+                            <div className='font-jekobold text-black text-2xl'>
+                                Une question ? Une remarques ? <br />{' '}
+                                <span className='text-[#ff4140] font-jekobold'>Laisse un commentaire</span>
                             </div>
+                            <div className='flex flex-col items-center justify-center mt-8 w-full'>
+                                <div className='flex items-center space-x-8 w-full'>
+                                    <Input
+                                        onChange={fieldChangeHandler}
+                                        placeholder='Ton nom'
+                                        className='rounded-full bg-[#F6F6F6] border-[#979797] border p-8'
+                                        name='commenter-name'
+                                    />
+                                    <Input
+                                        placeholder='Ton email'
+                                        name='commenter-email'
+                                        onChange={fieldChangeHandler}
+                                        className='rounded-full bg-[#F6F6F6] border-[#979797] border p-8'
+                                    />
+                                </div>
 
-                            <Textarea
-                                placeholder='Ton commentaire'
-                                rows={8}
-                                className='bg-[#F6F6F6] border-[#979797] border px-8 mt-8 rounded-2xl'
-                            />
-                            <div className='w-full flex items-center justify-end mt-8'>
-                                <Button className='text-white bg-[#ff4140] font-jekobold rounded-full'>Valider</Button>
+                                <Textarea
+                                    placeholder='Ton commentaire'
+                                    name='commenter-message'
+                                    rows={8}
+                                    className='bg-[#F6F6F6] border-[#979797] border px-8 mt-8 rounded-2xl'
+                                    onChange={fieldChangeHandler}
+                                />
+                                <div className='w-full flex items-center justify-end mt-8'>
+                                    <Button type='submit' className='text-white bg-[#ff4140] font-jekobold rounded-full'>
+                                        Valider
+                                    </Button>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </form>
+                    )}
                 </div>
 
                 <div className='w-full flex flex-col items-center justify-center mt-24'>
--- src/lib/wordpress/wp-client.ts
+++ src/lib/wordpress/wp-client.ts
@@ -1,124 +1,134 @@
-import { WP_REST_API_Post as Post, WP_REST_API_Categories, WP_REST_API_Category, WP_REST_API_Tag, WP_REST_API_Tags } from "wp-types";
- 
-const API_BASE_URL =
-  process.env.WORDPRESS_API_BASE_URL ?? "https://hiway.blog/wp-json";
- 
-const API_VERSION = process.env.WORDPRESS_API_VERSION ?? "v2";
+import { WP_REST_API_Post as Post, WP_REST_API_Categories, WP_REST_API_Category, WP_REST_API_Tag, WP_REST_API_Tags } from 'wp-types';
+
+const API_BASE_URL = process.env.WORDPRESS_API_BASE_URL ?? 'https://hiway.blog/wp-json';
+
+const API_VERSION = process.env.WORDPRESS_API_VERSION ?? 'v2';
 const BASE_URL = `${API_BASE_URL}/wp/${API_VERSION}/`;
- 
+
 const DEFAULT_POSTS_PARAMS = {
-  per_page: 9,
-  page: 1,
-  search: "",
-  slug: <string[]>[],
-  // categories: <number[]>[],
-  // tags: <number[]>[],
+    per_page: 9,
+    page: 1,
+    search: '',
+    slug: <string[]>[],
+    // categories: <number[]>[],
+    // tags: <number[]>[],
 };
- 
+
 type GetPostsParams = Partial<typeof DEFAULT_POSTS_PARAMS>;
 
 export interface PostWithMedia extends Post {
     mediaUrl: string;
 }
- 
+
 export default class WpClient {
-  constructor(
-    private readonly username: string,
-    private readonly password: string,
-  ) {}
- 
-  async getPosts(params: Partial<typeof DEFAULT_POSTS_PARAMS> = {}): Promise<{
-    posts: PostWithMedia[];
-    totalPages: number;
-  }> {
-    if (!params.page) {
-      params.per_page = 99;
+    constructor(private readonly username: string, private readonly password: string) {}
+
+    async getPosts(params: Partial<typeof DEFAULT_POSTS_PARAMS> = {}): Promise<{
+        posts: PostWithMedia[];
+        totalPages: number;
+    }> {
+        if (!params.page) {
+            params.per_page = 99;
+        }
+
+        const queryParams = this.queryString({
+            ...DEFAULT_POSTS_PARAMS,
+            ...params,
+        });
+
+        const url = `${BASE_URL}posts${queryParams}`;
+        const response = await fetch(url);
+        const totalPagesHeader = response.headers.get('X-WP-TotalPages');
+        const totalPages = totalPagesHeader ? Number(totalPagesHeader) : 0;
+        const posts = await response.json();
+
+        const postsWithMedia = await Promise.all(
+            posts.map(async (post: Post) => {
+                const mediaUrl = await this.getMediaUrl(post.featured_media || 0);
+                return {
+                    ...post,
+                    mediaUrl,
+                };
+            }),
+        );
+
+        return {
+            posts: postsWithMedia,
+            totalPages,
+        };
     }
-    
-    const queryParams = this.queryString({
-      ...DEFAULT_POSTS_PARAMS,
-      ...params,
-    });
- 
-    const url = `${BASE_URL}posts${queryParams}`;
-    const response = await fetch(url);
-    const totalPagesHeader = response.headers.get('X-WP-TotalPages');
-    const totalPages = totalPagesHeader ? Number(totalPagesHeader) : 0;
-    const posts = await response.json();
 
-    const postsWithMedia = await Promise.all(
-        posts.map(async (post: Post) => {
-            const mediaUrl = await this.getMediaUrl(post.featured_media || 0);
-            return {
-                ...post,
-                mediaUrl,
-            };
-        })
-    );
- 
-    return {
-      posts: postsWithMedia,
-      totalPages,
-    };
-  }
- 
-  async getPost(id: number): Promise<Post> {
-    const response = await fetch(`${BASE_URL}posts/${id}`);
- 
-    return response.json();
-  }
+    async getPost(id: number): Promise<Post> {
+        const response = await fetch(`${BASE_URL}posts/${id}`);
 
-  async getCategory(id: number): Promise<WP_REST_API_Category> {
-    const response = await fetch(`${BASE_URL}categories/${id}`);
-    return response.json();
-  }
+        return response.json();
+    }
 
-  async getTag(id: number): Promise<WP_REST_API_Tag> {
-    const response = await fetch(`${BASE_URL}tags/${id}`);
-    return response.json();
-  }
- 
-  private getHeaders() {
-    const auth = this.createWordpressBasicAuthHeader(
-      this.username,
-      this.password,
-    );
- 
-    return {
-      Authorization: auth,
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    };
-  }
+    async postComment(postId: number, name: string, email: string, content: string): Promise<void> {
+        const response = await fetch(`${BASE_URL}comments`, {
+            method: 'POST',
+            headers: this.getHeaders(),
+            body: JSON.stringify({
+                post: postId,
+                author_name: name,
+                author_email: email,
+                content,
+            }),
+        });
 
-  async getMediaUrl(id: number): Promise<string> {
-    const response = await fetch(`${BASE_URL}media/${id}`);
-    const media = await response.json();
-    return media.source_url;
-  }
+        if (!response.ok) {
+            throw new Error('Failed to post comment error');
+        }
+    }
 
-  async getCategories(): Promise<WP_REST_API_Category[]> {
-    const response = await fetch(`${BASE_URL}categories`);
-    return response.json();
-  }
+    async getCategory(id: number): Promise<WP_REST_API_Category> {
+        const response = await fetch(`${BASE_URL}categories/${id}`);
+        return response.json();
+    }
 
-  async getTags(): Promise<WP_REST_API_Tag[]> {
-    const response = await fetch(`${BASE_URL}tags`);
-    return response.json();
-  }
- 
-  private createWordpressBasicAuthHeader(username: string, password: string) {
-    const buffer = Buffer.from(`${username}:${password}`, "binary");
-    const encoded = buffer.toString("base64");
- 
-    return `Basic ${encoded}`;
-  }
- 
-  private queryString(params: Record<string, string | string[] | number | number[]>) {
-    const queryParams = Object.keys(params)
-      .map((key) => `${key}=${encodeURIComponent(params[key].toString())}`)
-      .join("&");
- 
-    return queryParams ? `?${queryParams}` : "";
-  }
+    async getTag(id: number): Promise<WP_REST_API_Tag> {
+        const response = await fetch(`${BASE_URL}tags/${id}`);
+        return response.json();
+    }
+
+    private getHeaders() {
+        const auth = this.createWordpressBasicAuthHeader(this.username, this.password);
+
+        return {
+            Authorization: auth,
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        };
+    }
+
+    async getMediaUrl(id: number): Promise<string> {
+        const response = await fetch(`${BASE_URL}media/${id}`);
+        const media = await response.json();
+        return media.source_url;
+    }
+
+    async getCategories(): Promise<WP_REST_API_Category[]> {
+        const response = await fetch(`${BASE_URL}categories`);
+        return response.json();
+    }
+
+    async getTags(): Promise<WP_REST_API_Tag[]> {
+        const response = await fetch(`${BASE_URL}tags`);
+        return response.json();
+    }
+
+    private createWordpressBasicAuthHeader(username: string, password: string) {
+        const buffer = Buffer.from(`${username}:${password}`, 'binary');
+        const encoded = buffer.toString('base64');
+
+        return `Basic ${encoded}`;
+    }
+
+    private queryString(params: Record<string, string | string[] | number | number[]>) {
+        const queryParams = Object.keys(params)
+            .map((key) => `${key}=${encodeURIComponent(params[key].toString())}`)
+            .join('&');
+
+        return queryParams ? `?${queryParams}` : '';
+    }
 }
